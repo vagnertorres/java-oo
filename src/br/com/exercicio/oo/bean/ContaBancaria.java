b6ec1f658d9847a93111e8c1ff41fd697841710e
Vagner Torres <esdras.torres.vt@gmail.com>
2017-06-05 23:45:01 -0300

    adcionando novo exercício

---
+++ src/br/com/exercicio/oo/bean/ContaBancaria.java
@@ -0,0 +1,42 @@
+package br.com.exercicio.oo.bean;
+
+public abstract class ContaBancaria implements java.io.Serializable {
+	
+	public double saldo;
+	public int agencia;
+	public String numeroConta;
+	
+	public ContaBancaria() {}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+	
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}	
+	
+	public void sacar(double valor) {
+		this.saldo -= valor;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+}
